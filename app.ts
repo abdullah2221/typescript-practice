77ec69204fca97103fa2718293fcd11b54544a71
Abdullah <72508253+abdullah2221@users.noreply.github.com>
2023-09-15 23:24:51 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 // 2. Personal Message: Store a person’s name in a variable, and print a message to that person. 
 // Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 console.log(" Exersise 2")
-const personName = "helo AbdUllAh";
+const personName = "Hello Abdullah, would you like to learn some Typescript  today?";
 
 
 // 3 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
